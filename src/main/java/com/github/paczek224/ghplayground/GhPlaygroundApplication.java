f2a79d4680d4ce063fe843fa99fdef757ba3f3d0
Lukasz PACZEK <paczeklukasz90@lyreco.com>
2026-06-17 10:59:08 +0200

    init

---
+++ src/main/java/com/github/paczek224/ghplayground/GhPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.github.paczek224.ghplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhPlaygroundApplication.class, args);
+    }
+
+}
